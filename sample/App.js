78faa323352e169cfa470aeb019ea2378360c3e6
박태준 <puze8681@loplat.com>
2021-08-20 20:04:20 +0900

    Switch Component 추가

--- sample/App.js
+++ sample/App.js
@@ -1,112 +1,75 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
+import React, { useState } from "react";
+import { SafeAreaView, Text, StatusBar, View, Switch, StyleSheet } from "react-native";
+import Toast from 'react-native-easy-toast';
 
-import React from 'react';
-import type {Node} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
-
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-
-const Section = ({children, title}): Node => {
-  const isDarkMode = useColorScheme() === 'dark';
-  return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
-    </View>
-  );
-};
-
-const App: () => Node = () => {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+const App = () => {
+  const locationSwitchText = "위치 기반 서비스 동의"
+  const marketingSwitchText = "마케팅 서비스 동의"
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar barStyle={isDarkMode ? 'light-content' : 'dark-content'} />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.js</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
+    <SafeAreaView style={appStyles.container}>
+      <StatusBar
+        barStyle={'light-content'}
+        backgroundColor="#000000" />
+      <SwitchComponent type={locationSwitchText} />
+      <SwitchComponent type={marketingSwitchText} />
     </SafeAreaView>
   );
-};
+}
 
-const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-  },
-  highlight: {
-    fontWeight: '700',
-  },
+const SwitchComponent = (props) => {
+  let toastRef;
+  const showToast = function(type, isEnabled){
+    const text = type + " permission is " + isEnabled
+    console.log(text)
+    toastRef.show(text)
+  }
+  const switchType = props.type
+  const [isEnabled, setIsEnabled] = useState(false);
+  const toggleSwitch = () => {
+    console.log(switchType)
+    console.log(isEnabled)
+    if (switchType === "location") {
+      console.log("1")
+    } else if (switchType === "marketing") {
+      console.log("2")
+    }
+    console.log(isEnabled)
+    showToast(switchType, isEnabled)
+    setIsEnabled(previousState => !previousState);
+    console.log(isEnabled)
+  }
+
+
+  return (
+    <SafeAreaView style={switchStyles.container}>
+      <Text>{switchType}</Text>
+      <Switch
+        trackColor={{ false: "#767577", true: "#81b0ff" }}
+        thumbColor={isEnabled ? "#f5dd4b" : "#f4f3f4"}
+        ios_backgroundColor="#3e3e3e"
+        onValueChange={toggleSwitch}
+        value={isEnabled}
+      />
+      <Toast ref={(toast) => toastRef = toast} position={'bottom'} />
+    </SafeAreaView>
+  )
+}
+
+const appStyles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center"
+  }
+});
+
+const switchStyles = StyleSheet.create({
+  container: {
+    flexDirection:'column',
+    alignItems: "center",
+    justifyContent: "flex-start"
+  }
 });
 
 export default App;
